afec5f22d61465c3abc2ac8ea0323392f4cdd0db
jasmine-aonach <jasmine@aonach.com>
2022-06-20 12:24:51 +0100

    finished authentication

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -1,11 +1,16 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/UIElements/Button';
 import Modal from '../../shared/components/UIElements/Modal';
 import Map from '../../shared/components/UIElements/Map';
+import { AuthContext } from '../../shared/context/auth-context';
 
 const PlaceItem = props => {
+
+  const auth = useContext(AuthContext);
+
   const [showMap, setShowMap] = useState(false);
+
   const [showConfirmModal, setShowConfirmModal] = useState(false);
 
   const openMapHandler = () => setShowMap(true);
@@ -74,10 +79,14 @@ const PlaceItem = props => {
             <Button inverse onClick={openMapHandler}>
               VIEW ON MAP
             </Button>
+            {auth.isLoggedIn && (
             <Button to={`/places/${props.id}`}>EDIT</Button>
+            )}
+            {auth.isLoggedIn && (
             <Button danger onClick={showDeleteWarningHandler}>
               DELETE
             </Button>
+            )}
           </div>
         </Card>
       </li>
--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useContext } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/UIElements/Input';
 import Button from '../../shared/components/UIElements/Button';
 import {VALIDATOR_EMAIL,VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from '../../shared/components/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
+import { AuthContext } from '../../shared/context/auth-context';
 
 const Auth = () => {
-    const [isLoginMode, setIsLoginMode] = useState(true);
+  const auth = useContext(AuthContext); 
+
+  const [isLoginMode, setIsLoginMode] = useState(true);
   
-    const [formState, inputHandler, setFormData] = useForm(
+  const [formState, inputHandler, setFormData] = useForm(
       {
         email: {
           value: '',
@@ -49,6 +52,7 @@ const Auth = () => {
     const authSubmitHandler = event => {
       event.preventDefault();
       console.log(formState.inputs);
+      auth.login();
     };
   
     return (
